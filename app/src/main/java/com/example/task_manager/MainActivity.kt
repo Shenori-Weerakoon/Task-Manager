f999ba47047383b73fb3091d0001e804981dbaab
shenori <it22150448@my.sliit.lk>
2024-05-11 20:38:20 +0530

    implement add new task function

--- app/src/main/java/com/example/task_manager/MainActivity.kt
+++ app/src/main/java/com/example/task_manager/MainActivity.kt
@@ -5,16 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.example.task_manager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var taskViewModel: TaskViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+        binding.newTaskButton.setOnClickListener{
+            NewTaskSheet().show(supportFragmentManager, "newTaskTag")
+        }
+
+        taskViewModel.name.observe(this){
+            binding.taskName.text = String.format("Task Name: %s", it)
+        }
+
+        taskViewModel.desc.observe(this){
+            binding.taskDesc.text = String.format("Task Desc: %s", it)
         }
     }
 }
