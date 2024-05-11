d8ee782ec2c79003f7984670f93f8f2e386af288
shenori <it22150448@my.sliit.lk>
2024-05-11 23:34:30 +0530

    implement update function

--- app/src/main/java/com/example/task_manager/MainActivity.kt
+++ app/src/main/java/com/example/task_manager/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.task_manager
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
 import com.example.task_manager.databinding.ActivityMainBinding
 
@@ -19,15 +16,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
         binding.newTaskButton.setOnClickListener{
-            NewTaskSheet().show(supportFragmentManager, "newTaskTag")
+            NewTaskSheet(null).show(supportFragmentManager, "newTaskTag")
         }
 
-        taskViewModel.name.observe(this){
-            binding.taskName.text = String.format("Task Name: %s", it)
-        }
-
-        taskViewModel.desc.observe(this){
-            binding.taskDesc.text = String.format("Task Desc: %s", it)
-        }
     }
 }
--- app/src/main/java/com/example/task_manager/NewTaskSheet.kt
+++ app/src/main/java/com/example/task_manager/NewTaskSheet.kt
@@ -1,17 +1,15 @@
 package com.example.task_manager
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
-import com.example.task_manager.databinding.ActivityMainBinding
 import com.example.task_manager.databinding.FragmentNewTaskSheetBinding
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class NewTaskSheet : BottomSheetDialogFragment() {
+class NewTaskSheet(var taskItem: TaskItem?) : BottomSheetDialogFragment() {
 
     private lateinit var binding: FragmentNewTaskSheetBinding
     private lateinit var taskViewModel: TaskViewModel
@@ -20,6 +18,19 @@ class NewTaskSheet : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val activity = requireActivity()
+
+        if (taskItem != null)
+        {
+            binding.taskTitle.text = "Edit Task"
+            val editable = Editable.Factory.getInstance()
+            binding.name.text = editable.newEditable(taskItem!!.name)
+            binding.desc.text = editable.newEditable(taskItem!!.desc)
+        }
+        else
+        {
+            binding.taskTitle.text = "New Task"
+        }
+
         taskViewModel = ViewModelProvider(activity).get(TaskViewModel::class.java)
         binding.saveButton.setOnClickListener {
             saveAction()
@@ -37,8 +48,19 @@ class NewTaskSheet : BottomSheetDialogFragment() {
     }
 
     private fun saveAction() {
-        taskViewModel.name.value = binding.name.text.toString()
-        taskViewModel.desc.value = binding.desc.text.toString()
+        val name = binding.name.text.toString()
+        val desc = binding.desc.text.toString()
+
+        if (taskItem == null)
+        {
+            val newTask = TaskItem(name,desc,null,null )
+            taskViewModel.addTaskItem(newTask)
+        }
+        else
+        {
+            taskViewModel.updateTaskItem(taskItem!!.id, name, desc, null)
+        }
+
         binding.name.setText("")
         binding.desc.setText("")
         dismiss()
